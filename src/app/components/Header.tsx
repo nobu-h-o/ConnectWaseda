1a8e2822425def8db08b33bc20c9c840a536c2f4
Nobuhiro Oto <nobuhirooto@gmail.com>
2025-01-11 03:19:42 +0900

    init

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Link from 'next/link';
+
+const navItems = {
+  '/about': {
+    name: 'About Us',
+  },
+  '/FAQ': {
+    name: 'FAQ',
+  },
+  '/Contact': {
+    name: 'Contact',
+  },
+  '/login': {
+    name: 'Login',
+  },
+};
+
+
+export default function Header() {
+  return (
+    <AppBar
+      position="fixed"
+      color="transparent"
+      elevation={0}
+      className="z-50"
+    >
+      <Toolbar className="flex justify-between items-center">
+        <Typography variant="h3" component="div" className="text-white">
+          Waseders Unite
+        </Typography>
+        <div className="flex items-center space-x-4">
+          {Object.entries(navItems).map(([path, { name }]) => (
+            <Link
+              key={path}
+              href={path}
+              className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex items-center relative py-1 px-2 text-white"
+            >
+              {name}
+            </Link>
+          ))}
+          <Button variant="contained" color="primary" sx={{
+                backgroundColor: 'crimson',
+                color: 'white',
+                '&:hover': {
+                    backgroundColor: '#a80000', // Darker shade for hover effect
+                },
+            }}>
+            Sign Up
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
