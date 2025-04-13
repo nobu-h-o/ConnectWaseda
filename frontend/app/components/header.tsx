894e50f99216864cfb2fabdf415046e46d1a670f
Nobuhiro Oto <nobuhirooto@gmail.com>
2025-04-13 23:10:54 +0900

    header fix

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -14,17 +14,6 @@ export default function Header() {
           ConnectWaseda
         </div>
 
-        <div className="flex items-center space-x-10">
-          <p className="text-white cursor-pointer">About Us</p>
-          <p className="text-white cursor-pointer">FAQ</p>
-          <p className="text-white cursor-pointer">Contact</p>
-
-          <Link href="/signin">
-            <div className="bg-[rgba(160,167,224,0.72)] px-6 py-3 rounded-full shadow transition duration-200 hover:shadow-lg hover:brightness-110 cursor-pointer">
-              <p className="text-white">Sign In</p>
-            </div>
-          </Link>
-        </div>
         <p>About Us</p>
         <p>FAQ</p>
         <p>Contact</p>
