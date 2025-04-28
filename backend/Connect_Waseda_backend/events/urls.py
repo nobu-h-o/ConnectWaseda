28de0fcd96c08c08d882238ddf8df6fb4bdb8e5d
Daniel Hu <dan.hu@mail.utoronto.ca>
2025-04-28 15:42:20 +0900

    added start+endtimes to events

--- backend/Connect_Waseda_backend/events/urls.py
+++ backend/Connect_Waseda_backend/events/urls.py
@@ -4,5 +4,7 @@ from .views import EventListCreate, EventDetail
 urlpatterns = [
     path("",        EventListCreate.as_view(), name="events-list-create"),
     path("<int:pk>/", EventDetail.as_view(),   name="events-detail"),
+    #path("upcoming/", UpcomingEventList.as_view(), name="events-upcoming"),
+    #path("mine/",     MyEventList.as_view(),      name="events-mine"),
 ]
 
