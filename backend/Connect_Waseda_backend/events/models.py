2b5130584a5c4a90e689e7ce2a8392e8f2f8e5c9
master3blade <brendon.gundam@gmail.com>
2025-05-14 17:29:22 +0900

    add club_rep
    add club_rep

--- backend/Connect_Waseda_backend/events/models.py
+++ backend/Connect_Waseda_backend/events/models.py
@@ -14,6 +14,7 @@ class Event(models.Model):
     )
     title       = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    club_rep = models.BooleanField(default=True)
     category = models.TextField(blank=True)
     language = models.TextField(blank=True)
 
