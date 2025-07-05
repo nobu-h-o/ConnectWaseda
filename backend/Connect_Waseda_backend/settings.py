3a9b814fbb9fce3259e1633b88f79c1e4dd2ee16
Daniel Hu <dan.hu@mail.utoronto.ca>
2025-07-05 16:48:00 +0900

    fixed CORS_origin

--- backend/Connect_Waseda_backend/settings.py
+++ backend/Connect_Waseda_backend/settings.py
@@ -87,14 +87,16 @@ MIDDLEWARE = [
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    # "http://localhost:3000", 
-    "https://connectwaseda.vercel.app/home", # Next.js development server URL
+     
+    "https://connectwaseda.vercel.app", # Next.js development server URL
+    "http://localhost:3000",
 ]
 
 # CSRF settings
 CSRF_TRUSTED_ORIGINS = [
     # "http://localhost:3000",  # Next.js development server URL
-    "https://connectwaseda.vercel.app/home",
+    "https://connectwaseda.vercel.app",
+    "http://localhost:3000", 
 ]
 CSRF_COOKIE_SAMESITE = 'Lax'
 CSRF_COOKIE_HTTPONLY = False  # Must be False to allow JavaScript to read the token
